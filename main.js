68094b2ccc2b3366faaa40943426ef4e58854946
Gianluca-Basili <128229910+Gianluca-Basili@users.noreply.github.com>
2023-05-30 18:50:08 +0200

    esercizio finito

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-let  lista = ['uova', 'guanciale','pepe','pecorino','parmiggiano']
+let list = [
+'uova',
+'guanciale',
+'pecorino',
+'pepe',
+'pasta',
+'parmiggiano'
+];
+
+console.log(list)
+
+const list_dom = document.getElementById('list');
+console.log(list_dom);
+
+let i=0;
+while(i<list.length){
+    console.log(list[i]);
+    let element = document.createElement('li');
+    element.innerText = list[i];
+    list_dom.append(element);
+    i++;
+}
--- readme.md
+++ readme.md
@@ -1,5 +1,5 @@
 PROBLEMA: lista della spesa con ciclo while
 
-1- creao un array coon la lista della spesa
+1- creao un array con la lista della spesa
 2- creare un elemnto dove inserisco a video la lista 
 3-creo un ciclo while
